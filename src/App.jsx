223946e939d331c6799e138526d01e580eeb3a6e
Shivansh-soni <shivanshsoni.indore@gmail.com>
2022-10-11 00:57:21 +0530

    Added setTimeout()

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
 import "./App.css";
 import "@lottiefiles/lottie-player";
-import React, { useEffect } from "react";
+import An1 from "./An1.json";
+import React, { useEffect, useState, useRef } from "react";
 import { create } from "@lottiefiles/lottie-interactivity";
-
 function App() {
+  const [loading, setLoading] = useState(false);
+  const myRef = React.useRef();
   useEffect(() => {
-    create({
-      player: "#firstLottie",
-      container: "#App",
-      mode: "scroll",
-      actions: [
-        {
-          visibility: [0, 1],
-          type: "seek",
-          frames: [-85, 155],
-        },
-      ],
-    });
+    const timer = setTimeout(() => {
+      create({
+        container: "#App",
+        player: "#firstLottie",
+        mode: "scroll",
+        actions: [
+          {
+            visibility: [0, 1],
+            type: "seek",
+            frames: [-85, 155],
+          },
+          setLoading(true),
+        ],
+      });
+    }, 1000);
+    return () => clearTimeout(timer);
   });
+
   return (
     <div className="App" id="App">
       {/* <Lottie id="firstLottie" animationData={An1} loop={false}></Lottie> */}
       <lottie-player
-        mode="loop"
+        ref={myRef}
         id="firstLottie"
+        style={{ height: "150vh" }}
         src="https://uploads-ssl.webflow.com/60c8f557ec7fdd963afcfaad/60d0ac70ea30404c095e6b75_hero-headset_v7.1.json"
-        style={{ width: "105%", height: "150vh" }}
       ></lottie-player>
     </div>
   );
